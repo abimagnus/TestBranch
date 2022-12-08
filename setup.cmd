b0023149c0d04101d9b4265dfd331041502567b7
abimagnus <abilashv@hotmail.com>
2022-12-08 18:04:31 +0000

    first commit

---
+++ setup.cmd
@@ -0,0 +1,6 @@
+echo "# BranchTest" >> README.md
+git init
+git add README.md
+git commit -m "first commit" git branch -M main
+git remote add origin https://github.com/abimagnus/TestBranch.git
+git push -u origin main
